638b7bfe48794a0170dbf647b4d9b82ee60ef767
HTMonkeyG <3254339747@qq.com>
2025-06-19 13:38:25 +0800

    Update function offsets due to the update of Sky.

--- src/offsets.h
+++ src/offsets.h
@@ -3,11 +3,22 @@
 
 #include "aliases.h"
 
+#define VERSION_SKY 2
+
+#if VERSION_SKY == 1
 // 0.14.2.325380, 2025.6.11
 static const u64 offset_SkyCamera_update = 0x019DBA80ull
   , offset_SkyCamera_updateUI = 0x00779A50ull
   , offset_SkyCamera__updateParams = 0x019DD660ull
   , offset_SkyCamera_setState = 0x00000000ull
   , offset_Player_getPos = 0x01C02ED0ull;
+#elif VERSION_SKY == 2
+// 0.14.3.326140, 2025.6.19
+static const u64 offset_SkyCamera_update = 0x01A1DCF0ull
+  , offset_SkyCamera_updateUI = 0x007A8D60ull
+  , offset_SkyCamera__updateParams = 0x01A1F8D0ull
+  , offset_SkyCamera_setState = 0x00000000ull
+  , offset_Player_getPos = 0x01C45070ull;
+#endif
 
 #endif
