70ad48631daf44528048297ec88529859b4eed56
HTMonkeyG <3254339747@qq.com>
2025-07-18 01:42:55 +0800

    fix: Fix incorrect unit conversion during simulated fireworks acceleration.

--- src/fpv/elytra.c
+++ src/fpv/elytra.c
@@ -40,7 +40,7 @@ static inline void elytraFirework(v4f mDelta, f32 timeElapsed) {
   );
 
   // Convert m/gt to m/s and apply accelerate.
-  gElytra.vel = v4fadd(gElytra.vel, v4fscale(tmp1, mDelta.z * timeElapsed * 20.0f));
+  gElytra.vel = v4fadd(gElytra.vel, v4fscale(tmp1, mDelta.z * timeElapsed * 400.0f));
 }
 
 /**
@@ -50,54 +50,6 @@ static inline void elytraFirework(v4f mDelta, f32 timeElapsed) {
  * convert to second.
  */
 static inline void elytraTravel(f32 timeElapsed) {
-  /*v4f vel = gElytra.vel
-    , view = calculateViewVector(gElytra.rot)
-    , tmpVec;
-  f32 timeGt = timeElapsed * 20.0f
-    , viewXZLen = sqrtf(view.x * view.x + view.z * view.z)
-    , horizonSpeed = sqrtf(vel.x * vel.x + vel.z * vel.z)
-    , cosPitch2 = cosf(gElytra.rot.x)
-    // Gravitational acceleration.
-    , gravity = -0.08f * timeGt
-    , tmp;
-
-  cosPitch2 *= cosPitch2;
-  vel.y += -32.0f * timeElapsed * (1.0f - 0.75f * cosPitch2);
-
-  if (viewXZLen > 0.0f) {
-    if (vel.y < 0) {
-      // Apply diving forward speed.
-      tmp = vel.y * -0.1f * cosPitch2 * timeGt;
-      vel = v4fadd(
-        vel,
-        v4fnew(view.x * tmp / viewXZLen, tmp, view.z * tmp / viewXZLen, 0));
-    }
-
-    if (gElytra.rot.y > 0.0f) {
-      // Apply speed improvement.
-      tmp = horizonSpeed * sinf(gElytra.rot.y) * 0.04f * timeGt;
-      vel = v4fadd(
-        v4fnew(-view.x * tmp / viewXZLen, tmp * 3.2f, -view.z * tmp / viewXZLen, 0),
-        vel);
-    }
-
-    // Apply gliding descent acceleration.
-    tmpVec = v4fnew(
-      view.x / viewXZLen * horizonSpeed - vel.x,
-      0,
-      view.z / viewXZLen * horizonSpeed - vel.z,
-      0);
-    tmpVec = v4fscale(tmpVec, timeGt * 0.1f);
-    vel = v4fadd(vel, tmpVec);
-  }
-
-  // Apply air resistance.
-  tmpVec = v4fmul(vel, v4fscale(v4fnew(0.01f, 0.02f, 0.01f, 0), timeGt));
-  gElytra.vel = v4fsub(vel, tmpVec);
-
-  // Do movement.
-  gElytra.pos = v4fadd(gElytra.pos, v4fscale(gElytra.vel, timeElapsed));*/
-
   v4f view = calculateViewVector(gElytra.rot)
     // Convert to m/gt.
     , vel = v4fscale(gElytra.vel, 1 / 20.0f)
